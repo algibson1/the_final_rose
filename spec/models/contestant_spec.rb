1c98858c5031c8b0059b4ce9908308c4842651c5
Artemy Gibson <artemygibson@gmail.com>
2024-01-15 14:13:02 -0800

    add relationships to outings, test methods season_number and season_description

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -3,5 +3,27 @@ require "rails_helper"
 RSpec.describe Contestant, type: :model do
   describe "relationships" do
     it {should belong_to :bachelorette}
+    it { should have_many :contestant_outings }
+    it { should have_many(:outings).through(:contestant_outings) }
+  end
+
+  describe "instance methods" do
+    describe "#season_number" do
+      it "returns the season number the contestant was on" do
+        bachelorette = Bachelorette.create!(name: "Sally Ride", season_number: 2, season_description: "Quazy")
+        contestant1 = bachelorette.contestants.create!(name: "Brad Tailorman", age: 42, hometown: "Candy Mountain Ridge")
+
+        expect(contestant1.season_number).to eq(2)
+      end
+    end
+
+    describe "#season_description" do
+      it "returns the season number the contestant was on" do
+        bachelorette = Bachelorette.create!(name: "Sally Ride", season_number: 2, season_description: "Quazy")
+        contestant1 = bachelorette.contestants.create!(name: "Brad Tailorman", age: 42, hometown: "Candy Mountain Ridge")
+
+        expect(contestant1.season_description).to eq("Quazy")
+      end
+    end
   end
 end
